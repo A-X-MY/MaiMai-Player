0439f055f1f11e7ca7911e66ee8e3daa118caba1
你的麦哥哥 <119749915+A-X-MY@users.noreply.github.com>
2023-06-21 10:50:20 +0800

    Add files via upload

---
+++ MaiMai-Player/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useTheme } from "@/hooks/useTheme";
+import defaultComm from "@/layouts/default.vue";
+
+// 使用主题
+useTheme()
+</script>
+<template>
+	<defaultComm />
+	<!-- <router-view /> -->
+</template>
+
+
